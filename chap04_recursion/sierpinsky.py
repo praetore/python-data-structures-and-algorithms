b15dd1e11740cd1a7a2ff15b6b8f025e53e32f1b
darryl <d.amatsetam@gmail,com>
2015-03-17 12:47:54 +0100

    added sierpinsky triangle

---
+++ chap04_recursion/sierpinsky.py
@@ -0,0 +1,50 @@
+import turtle
+
+__author__ = 'darryl'
+
+
+def draw_triangle(points, color, t):
+    t.fillcolor(color)
+    t.up()
+    t.goto(points[0][0], points[0][1])
+    t.down()
+    t.begin_fill()
+    t.goto(points[1][0], points[1][1])
+    t.goto(points[2][0], points[2][1])
+    t.goto(points[0][0], points[0][1])
+    t.end_fill()
+
+
+def mid(p1, p2):
+    return (p1[0] + p2[0]) / 2, (p1[1] + p2[1]) / 2
+
+
+def sierpinski(points, degree, t):
+    colormap = ['blue', 'red', 'green', 'white', 'yellow',
+                'violet', 'orange']
+    draw_triangle(points, colormap[degree], t)
+    if degree > 0:
+        sierpinski([points[0],
+                    mid(points[0], points[1]),
+                    mid(points[0], points[2])],
+                   degree - 1, t)
+        sierpinski([points[1],
+                    mid(points[0], points[1]),
+                    mid(points[1], points[2])],
+                   degree - 1, t)
+        sierpinski([points[2],
+                    mid(points[2], points[1]),
+                    mid(points[0], points[2])],
+                   degree - 1, t)
+
+
+def main():
+    t = turtle.Turtle()
+    win = turtle.Screen()
+    points = [[-100, -50], [0, 100], [100, -50]]
+    sierpinski(points, 3, t)
+    win.exitonclick()
+
+
+if __name__ == '__main__':
+    main()
