43a37b8fd14f3cf2553f6cd5f0a31e7a6fc80d11
darryl <d.amatsetam@gmail,com>
2015-03-17 12:47:15 +0100

    added more variation in branch leaf length and rotation

--- chap04_recursion/turtletree.py
+++ chap04_recursion/turtletree.py
@@ -1,30 +1,44 @@
 from turtle import Turtle
 from turtle import Screen
+import random
 
 __author__ = 'darryl'
 
 
+def get_branch_len(branch_len):
+    res = branch_len - random.randint(5, 15)
+    return res if res > 0 else 5
+
+
 def tree(branch_len, t, w=12, c=2):
+    i = random.randint(15, 45)
     if branch_len > 5:
         t.width(w-c)
         t.forward(branch_len)
-        t.right(20)
-        tree(branch_len - 15, t, w=w-c)
-        t.left(40)
-        tree(branch_len - 15, t, w=w-c)
-        t.right(20)
+        t.right(i)
+        tree(get_branch_len(branch_len), t, w=w-c if w > c else c)
+        t.left(i*2)
+        tree(get_branch_len(branch_len), t, w=w-c if w > c else c)
+        t.right(i)
+        tree(get_branch_len(branch_len), t, w=w-c if w > c else c)
         t.backward(branch_len)
+    if branch_len < 15:
+        t.width(4)
+        t.color("green")
+    else:
+        t.color("brown")
 
 
 def main():
     t = Turtle()
     my_win = Screen()
-    t.width(10)
+    t.width(12)
+    t.speed(10)
     t.left(90)
     t.up()
     t.backward(100)
     t.down()
-    t.color("green")
+    t.color("brown")
     tree(75, t)
     my_win.exitonclick()
 
